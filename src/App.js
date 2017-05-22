9398a617932a42a3d6ad625b9fe0d4416f1859ac
Akash <akash.raju@outlook.com>
2017-05-22 15:21:30 -0400

    break the links into 4 components

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-     
         <main className="expanded row">
           <Article />
           <Ad /> 
